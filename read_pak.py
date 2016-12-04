c6513294debf624ff73bc2213567c8567cdd5b06
Nanamachi <town7.haruki@gmail.com>
2016-12-04 09:18:26 +0900

    ADDED func read_LSB into read_pak : now can read Little-Endian binary easyly

--- read_pak.py
+++ read_pak.py
@@ -1,18 +1,32 @@
-#!env python
+# -*- coding: utf-8 -*-
 import sys
 import struct
 from PIL import Image
 
 class PakObj():
-
     def __init__(self, path):
+        # http://www.ajisaba.net/python/binary.html
         self._f = open(path, 'rb')
         self._path = path
-        self._read()
-
-    def _read(self):
-        # http://www.ajisaba.net/python/binary.html
         self._bin = self._f.read()
+    def read_LSB(self, binary, fmt):
+        if fmt == 'uint8':
+            packfmt = 'B'
+            packlen = 1
+        elif fmt == 'uint16':
+            packfmt = 'H'
+            packlen = 2
+        elif fmt == 'uint32':
+            packfmt = 'I'
+            packlen = 4
+        else:
+            raise
+
+        return [struct.unpack(packfmt, binary[0:packlen]), binary[packlen:]]
+
+def main(path):
+    pak = PakObj(path)
+    return pak._bin
 
 if __name__ == '__main__':
-    pak = PakObj(sys.argv[1])
+    print(main(sys.argv[1]))
